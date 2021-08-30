4dec256329ee0ce779ab4bbfb86c65d00a61c68c
prabhat1510 <prabhatc@gmail.com>
2021-08-30 14:03:26 +0530

    Update with diff type of Method Ref eg

--- MainApplication.java
+++ MainApplication.java
@@ -8,12 +8,12 @@ import java.util.List;
 public class MainApplication {
 
     public static void main (String[] args){
-        Side side;
-        Order orderOne = new Order("AUD",15000,null);
-        Order orderTwo = new Order("AUD",5000,null);
-        Order orderThree = new Order("AUD",11000,null);
-        Order orderFour = new Order("AUD",25000,null);
-        Order orderFive = new Order("AUD",8000,null);
+        //Side side;
+        Order orderOne = new Order("AUD",15000);
+        Order orderTwo = new Order("AUD",5000);
+        Order orderThree = new Order("AUD",11000);
+        Order orderFour = new Order("AUD",25000);
+        Order orderFive = new Order("AUD",8000);
 
         List<Order> orderList = new ArrayList<Order>();
         orderList.add(orderOne);
@@ -26,6 +26,18 @@ public class MainApplication {
 
        System.out.println(orderList.toString());
        orderList.forEach(order->System.out.println(order.getAmount()));
+       //Static Method Reference Example
+       DisplayInformation display = Order::displayCurrency;
+       display.display();
+
+
+       //Reference to instance method
+        DisplayInformation displayInfo = orderOne::displayAmount;
+        displayInfo.display();
+
+        //Reference to constructor
+        OrderAmount orderAmount = Order::new;
+        orderAmount.getOrderAmount(51000);
 
     }
 }
