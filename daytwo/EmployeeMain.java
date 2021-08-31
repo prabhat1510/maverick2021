d6367fa313305bd3a59a9b92fd04e59c7549ec78
prachand4 <prachand4@publicissapient.com>
2021-08-31 10:10:44 +0530

    Updated with extra data

--- daytwo/EmployeeMain.java
+++ daytwo/EmployeeMain.java
@@ -1,5 +1,15 @@
 package training.daytwo.streams;
 
 public class EmployeeMain {
-        Employee emp = new Employee(1,"Mani",28,150000,"Manager","IT");
+        Employee empOne = new Employee(1,"Mani",28,150000,"Manager","IT");
+        Employee empTwo = new Employee(2,"Binay",29,160000,"Senior Associate","IT");
+        Employee empThree = new Employee(4,"Rahul",32,170000,"Senior Manager","Marketing");
+        Employee empFour = new Employee(3,"Abhishek",24,80000,"Associate","Finance");
+        Employee empFive = new Employee(5,"Rajesh",38,250000,"Director","IT");
+        Employee empSix = new Employee(6,"Venkat",29,100000,"Associate","Admin");
+        Employee empSeven = new Employee(4,"Ram",32,140000,"Senior Manager","Marketing");
+        Employee empEight= new Employee(3,"Robert",24,110000,"Associate","Finance");
+
+
+
 }
