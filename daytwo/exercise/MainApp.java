21cd564f4983d05b3ee6ec7d2d835aec37e00666
prachand4 <prachand4@publicissapient.com>
2021-08-31 17:16:33 +0530

    Updated today's class work

---
+++ daytwo/exercise/MainApp.java
@@ -0,0 +1,8 @@
+package training.daytwo.exercise;
+
+public class MainApp {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ daytwo/exercise/Product.java
@@ -0,0 +1,43 @@
+package training.daytwo.exercise;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private String category;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setProductName(String productName) {
+        this.name = productName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+}
