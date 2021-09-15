8bb308d444719f0f5be024d074eacfaea9b0d53e
prachand4 <prachand4@publicissapient.com>
2021-09-15 13:03:29 +0530

    Updated many to many example

--- daysix/jpatraining/src/main/java/jpatraining/manytomany/PhoneNumber.java
+++ daysix/jpatraining/src/main/java/jpatraining/manytomany/PhoneNumber.java
@@ -4,9 +4,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
 @Entity
+@Table(name = "CUST_PHONES")
 public class PhoneNumber {
     @Id
     @Column(name = "id", nullable = false)
